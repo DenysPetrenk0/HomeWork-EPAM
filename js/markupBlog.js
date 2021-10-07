d0ea5450e7f60840c2895d96c891d3095249d46f
DenysPetrenk0 <p.gesser2015@gmail.com>
2021-10-07 23:59:46 +0300

    delet default values

--- js/markupBlog.js
+++ js/markupBlog.js
@@ -81,13 +81,6 @@ function markupMain(value) {
         { name: "width", value: 560 },
       ],
     });
-    const imgIconImg = createElem({
-      nodeType: "img",
-      attribute: [
-        // { name: "src", value: elem.image.icon },
-        { name: "alt", value: "icon" },
-      ],
-    });
     const iconAuthorBlog = createElem({
       nodeType: "img",
       attribute: [
@@ -111,19 +104,15 @@ function markupMain(value) {
       text: "Read more",
       attribute: [{ name: "href", value: "" }],
     });
-    const audio = createElem({
-      nodeType: "audio",
-      attribute: [{ name: "controls", value: "controls" }],
-    });
+
     getCast(elem.id, idx);
+    getReviews(elem.id, idx);
 
     authorIconBlog.append(iconAuthorBlog);
     infoBlog.append(blogTitle);
-    // elem.audio ? infoBlog.append(audio) : null;
     infoBlog.append(blogText);
     infoBlog.append(readMore);
     infoBlog.append(authorIconBlog);
-    // elem.image.icon ? blogIconImg.append(imgIconImg) : null;
     blog.append(blogIconImg);
     blog.append(img);
     blog.append(infoBlog);
@@ -136,7 +125,6 @@ function markupMain(value) {
   searchIcon.append(imgIconSearch);
   search.append(searchIcon);
   container.append(search);
-  getReviews();
   section.append(containerSection);
   main.append(container);
   main.append(section);
@@ -190,7 +178,7 @@ function markupAuthorBlog(value, idx, id) {
     className: "author_name",
     text: value[0].name,
   });
-  getMovieDetails(id, idx);
+  getMovieDetails(id, idx, ".author_info");
 
   authorInfo.append(authorName);
   author.append(authorImg);
@@ -206,8 +194,8 @@ const getCast = (id, idx) => {
   );
 };
 
-function murkupCommentBlog(value) {
-  value.results.forEach((elem) => {
+function murkupCommentBlog(value, id, idx) {
+  if (value) {
     const blogComment = createElem({
       nodeType: "div",
       className: "blog_comment",
@@ -219,12 +207,12 @@ function murkupCommentBlog(value) {
     const commentText = createElem({
       nodeType: "p",
       className: "author_blog_text_comment",
-      text: elem.content,
+      text: value.content,
     });
     const commentTitle = createElem({
       nodeType: "h2",
       className: "author_blog_title",
-      text: elem.title,
+      text: value.title,
     });
     const readMore = createElem({
       nodeType: "a",
@@ -245,7 +233,7 @@ function murkupCommentBlog(value) {
     });
     const authorInfo = createElem({
       nodeType: "div",
-      className: "author_info",
+      className: "author_info_comment",
     });
     const authorImg = createElem({
       nodeType: "img",
@@ -261,30 +249,12 @@ function murkupCommentBlog(value) {
     const authorName = createElem({
       nodeType: "p",
       className: "author_name",
-      text: elem.author,
+      text: value.author,
     });
-    const comment = markupDescription();
 
-    arrStars = translatesRatingIntoStars(elem.author_details.rating);
+    getMovieDetails(id, idx, ".author_info_comment");
 
-    arrStars.forEach((item) => {
-      const divIconStar = createElem({
-        nodeType: "div",
-        className: "star_icon",
-      });
-      const imgIconStar = createElem({
-        nodeType: "img",
-        attribute: [
-          { name: "src", value: item },
-          { name: "alt", value: "icon" },
-        ],
-      });
-
-      divIconStar.append(imgIconStar);
-      comment.append(divIconStar);
-    });
     authorInfo.append(authorName);
-    authorInfo.append(comment);
     author.append(authorImg);
     author.append(authorInfo);
 
@@ -297,126 +267,13 @@ function murkupCommentBlog(value) {
 
     const containerSection = document.querySelectorAll(".container");
     containerSection[2].insertAdjacentElement("beforeend", blogComment);
-  });
+  }
 }
 
-const getReviews = () => {
-  fetchData(`550988/reviews?api_key=${API_KEY}&language=en-US&page=1`).then(
-    murkupCommentBlog
+const getReviews = (id, idx) => {
+  fetchData(`${id}/reviews?api_key=${API_KEY}&language=en-US&page=1`).then(
+    (result) => {
+      murkupCommentBlog(result.results[0], id, idx);
+    }
   );
 };
-
-const markupDescriptionBlog = (value, idx) => {
-  const div = createElem({
-    nodeType: "div",
-    className: "comment",
-  });
-  const icon = createElem({
-    nodeType: "div",
-    className: "comment_icon",
-  });
-  const date = createElem({
-    nodeType: "p",
-    className: "latest-posts__comment",
-    text: currentDate(value.release_date),
-  });
-  const oval = createElem({
-    nodeType: "p",
-    className: "oval",
-  });
-  const img = createElem({
-    nodeType: "img",
-    attribute: [
-      { name: "src", value: "./images/atoms/a-icon-comment.svg" },
-      { name: "alt", value: "icon" },
-    ],
-  });
-
-  const time = date.cloneNode(false);
-  const commetn = date.cloneNode(false);
-
-  time.textContent = `${value.runtime} min read`;
-  commetn.textContent = value.revenue;
-
-  icon.append(img);
-  div.append(date);
-  div.append(oval);
-  div.append(time);
-  div.append(oval.cloneNode(false));
-  div.append(icon);
-  div.append(commetn);
-
-  arrStars = translatesRatingIntoStars(value.vote_average);
-  arrStars.forEach((item) => {
-    const divIconStar = createElem({
-      nodeType: "div",
-      className: "star_icon",
-    });
-    const imgIconStar = createElem({
-      nodeType: "img",
-      attribute: [
-        { name: "src", value: item },
-        { name: "alt", value: "icon" },
-      ],
-    });
-
-    divIconStar.append(imgIconStar);
-    div.append(divIconStar);
-  });
-
-  const authorInfo = document.querySelectorAll(".author_info");
-  authorInfo[idx].insertAdjacentElement("beforeend", div);
-};
-
-function getMovieDetails(id, idx) {
-  fetchData(`${id}?api_key=${API_KEY}&language=en-US`).then((value) => {
-    markupDescriptionBlog(value, idx);
-  });
-}
-
-const month = [
-  "jan",
-  "feb",
-  "mar",
-  "apr",
-  "May",
-  "june",
-  "july",
-  "aug",
-  "sept",
-  "oct",
-  "nov",
-  "dec",
-];
-
-function currentDate(value) {
-  const date = new Date(Date.parse(value));
-  const year = date.getFullYear();
-  const wordMonth = month[date.getMonth() + 1];
-  const day = date.getDate();
-
-  return `${day} ${wordMonth}, ${year}`;
-}
-
-function movieRatingPercent(value) {
-  const retingPercent = value * 10;
-  return retingPercent;
-}
-
-function translatesRatingIntoStars(value) {
-  let reting = movieRatingPercent(value);
-  let newArr = [];
-  for (let i = 0; i < 5; i++) {
-    reting = reting - 20;
-    if (reting > 0) {
-      if (reting >= 20) {
-        newArr.push(fullStar);
-      } else {
-        newArr.push(halfStar);
-      }
-    } else {
-      newArr.push(emptyStar);
-    }
-  }
-  return newArr;
-}
