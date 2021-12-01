208d282e903ebeb2a2a45a08992e2d03bb93f171
Denys Petrenko <p.gesser2015@gmail.com>
2021-12-01 21:51:53 +0200

    HW_18: bug fix

--- js/mediator/btnAuthor/optionBtnAuthor.js
+++ js/mediator/btnAuthor/optionBtnAuthor.js
@@ -5,15 +5,6 @@ const refsBtnAuthor = {
   btnAuthorRight: document.querySelector(".content__vertical-btn"),
 };
 
-const checkButtons = (event) => {
-  const { className } = event.target;
-  if (className === "movie-info__list__btn") {
-    addMovieContent(event);
-  } else {
-    addMovieList(event);
-  }
-};
-
 const mediatorBtnAuthor = new Mediator(refsBtnAuthor.btnAuthor);
 const mediatorBtnAuthorRight = new Mediator(refsBtnAuthor.btnAuthorRight);
 mediatorBtnAuthor.addListener();
